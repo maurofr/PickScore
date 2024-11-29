b3c376f15a966428998b0f8ff631873e7490c418
Mauro Filomeno Rivero <m.filomenor@gmail.com>
2024-11-29 13:06:27 -0500

    Fix SLURM parameters

--- trainer/slurm_scripts/slurm_train.py
+++ trainer/slurm_scripts/slurm_train.py
@@ -101,10 +101,10 @@ def main(cfg: DictConfig) -> None:
         "slurm_job_name": cfg.slurm.job_name,
         "slurm_nodes": cfg.slurm.n_nodes,
         "slurm_additional_parameters": slurm_additional_parameters,
-        "slurm_cpus_per_task": 12,
+        "slurm_cpus_per_task": 4,
         "slurm_time": cfg.slurm.time_limit,
         "stderr_to_stdout": True,
-        "slurm_mem": "50GB",
+        "slurm_mem": "0",
     }
     executor.update_parameters(**slurm_kwargs)
 
